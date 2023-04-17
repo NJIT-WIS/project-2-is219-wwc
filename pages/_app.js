cc97feb2d81d8ddde975b7889d506f35d97fba59
dm594 <dm594@njit.edu>
2023-04-17 17:21:49 -0400

    fixed double layout.js appearing on page

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,6 @@ import Layout from '../components/layout'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
       <Component {...pageProps} />
-    </Layout>
   )
 }
