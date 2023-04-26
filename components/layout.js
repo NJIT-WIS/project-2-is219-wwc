2f0a969b397f3f6c5e7d39c6159c7eb1d12a519b
ah593 <ah593@njit.edu>
2023-04-26 13:01:28 -0400

    adding/creating opengraph and twitter metatags component

--- components/layout.js
+++ components/layout.js
@@ -21,6 +21,13 @@ export default function Layout({ children}) {
           console.log(`script loaded correctly, window.FB has been populated`)
         }
       />
+      <MetaTags
+        title="MyWebClass - Pioneering the Future of Education"
+        description="MyWebClass is a foundation committed to revolutionizing education by incorporating Agile and Lean principles into the classroom."
+        imageURL="https://example.com/path-to-your-image.jpg"
+        imageAlt="A description of the image"
+        url="https://example.com"
+      />
       <Header />
       <div className={styles.container}>
       <main>{children}</main>
