4e9ab260a18868429a22a6ac66c5fc16fb3c75bc
Demi Matos <dm594@njit.edu>
2023-04-28 14:16:50 -0400

    installed i18n required libraries

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
     experimental: {
       newNextLinkBehavior: false,
     },
-  };
-  
+
+  i18n: {
+    locales: ['en-US', 'es', 'fr'], // List of supported locales
+    defaultLocale: 'en-Us', // The default locale
+  },
+};
+ 
   module.exports = nextConfig;  
