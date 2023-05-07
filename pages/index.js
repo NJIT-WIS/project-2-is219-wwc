a153fe9d440fb62199fa0255561f6fea29cef330
ah593 <ah593@njit.edu>
2023-05-07 18:54:32 -0400

    fixed cookie consent so it populated using modal.js

--- pages/index.js
+++ pages/index.js
@@ -5,37 +5,72 @@ import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
 import Hero from "../components/hero";
-import CookieConsentBanner from "../components/cookieConsentBanner";
 import Modal from "../components/modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CallToAction from "../components/cta";
+import Cookies from "js-cookie";
 
 export default function Home({ allPostsData }) {
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
-  const openModal = () => {
-    setShowModal(true);
-  };
+  useEffect(() => {
+    if (!Cookies.get("cookieConsent")) {
+      setShowModal(true);
+    }
+  }, []);
 
   const closeModal = () => {
     setShowModal(false);
   };
+
+  const actions = (
+    <>
+      <button
+        className="bg-gray-500 text-white px-4 py-2 rounded mr-2"
+        onClick={() => {
+          closeModal();
+          Cookies.remove("cookieConsent");
+        }}
+      >
+        I Decline
+      </button>
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={() => {
+          closeModal();
+          Cookies.set("cookieConsent", "true", { expires: 365 });
+        }}
+      >
+        I Agree
+      </button>
+    </>
+  );
+
+  const modalContent = (
+    <p className="mb-4">
+      We use cookies to ensure that we give you the best experience on our
+      website. If you continue to use this site, we will assume that you are
+      happy with it.
+    </p>
+  );
+
   return (
     <>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <CookieConsentBanner />
       <Hero
         title="Example Card"
         description="This is an example card component in a Next.js application."
         image="/images/example.png"
       />
-      <Modal show={showModal} onClose={closeModal}>
-        <h2>Example Modal</h2>
-        <p>This is a basic modal component in a Next.js application.</p>
-        <button onClick={closeModal}>Close Modal</button>
-      </Modal>
+      <Modal
+        show={showModal}
+        onClose={closeModal}
+        title="Cookie Consent"
+        content={modalContent}
+        actions={actions}
+      />
     </>
   );
 }
