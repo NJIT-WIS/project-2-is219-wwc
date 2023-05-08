27c82e73d3dd9c5f9147a4df123169fbc93031f9
ah593 <ah593@njit.edu>
2023-05-08 05:45:14 -0400

    google analytics to track sales funnel

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Add your Google Analytics tracking code here */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-Q3QE4XE0VD"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-Q3QE4XE0VD');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
