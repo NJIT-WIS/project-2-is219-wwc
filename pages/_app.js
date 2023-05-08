9dfdbab79c4ee9bca18f00d1edf0ee0d9d1a25e4
ah593 <ah593@njit.edu>
2023-05-08 18:33:37 -0400

    setting up basic error tracking for client side

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,38 @@
 import '../styles/global.css'
 import Layout from '../components/layout'
 import '../styles/tailwind.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  useEffect(() => {
+    const handleError = (error) => {
+      gtag.event('js_error', {
+        event_category: 'Error',
+        event_label: `${error.message} at ${error.filename}:${error.lineno}:${error.colno}`,
+      });
+    };
+
+    window.addEventListener('error', handleError);
+    return () => {
+      window.removeEventListener('error', handleError);
+    };
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
