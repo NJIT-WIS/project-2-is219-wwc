8315626bb7316841cfa265b9df8d42b62c009080
ah593 <ah593@njit.edu>
2023-05-08 23:09:14 -0400

    updating sharable links to past playwright test

--- pages/post/[slug]/index.js
+++ pages/post/[slug]/index.js
@@ -28,6 +28,13 @@ function Post({ post }) {
   const postTitle = `${post.title} - MyWebClass`;
   const postDescription = ""; // Replace with 'post.summary' field from form data
   const postImageURL = urlFor(post.mainImage).url();
+  const openFacebookSharePopup = (url) => {
+    window.open(url, "facebook-share", "width=640,height=320");
+  };
+  const openTwitterSharePopup = (url) => {
+    window.open(url, "twitter-share", "width=640,height=320");
+  };
+
   return (
     <>
       <MetaTags
@@ -61,7 +68,7 @@ function Post({ post }) {
               className="lg:rounded"
             />
           </div>
-          <div className="flex py-2">
+          <div className="flex py-2 justify-between items-center">
             {post.author && post.author.image && (
               <Image
                 className="h-10 w-10 rounded-full mr-2 object-cover"
@@ -75,6 +82,50 @@ function Post({ post }) {
               <p className="font-semibold text-gray-700 text-sm">
                 {post.author ? post.author.name : "By: Unknown"}
               </p>
+            </div>
+            <div>
+              {/* Facebook Share Button */}
+              <button
+                className="bg-blue-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded"
+                onClick={() => {
+                  const postUrl = encodeURIComponent(
+                    `${baseUrl}/post/${post.slug}`
+                  );
+                  const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${postUrl}`;
+                  openFacebookSharePopup(facebookShareUrl);
+                }}
+              >
+                <svg
+                  className="w-5 h-5 fill-current"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
+                </svg>
+              </button>
+              {/* Twitter Share Button */}
+              <button
+                className="bg-blue-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded"
+                onClick={() => {
+                  const postUrl = encodeURIComponent(
+                    `${baseUrl}/post/${post.slug}`
+                  );
+                  const twitterShareUrl = `https://twitter.com/intent/tweet?url=${postUrl}&text=${encodeURIComponent(
+                    post.title
+                  )}`;
+                  openTwitterSharePopup(twitterShareUrl);
+                }}
+              >
+                <svg
+                  className="w-5 h-5 fill-current"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v-.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
+                </svg>
+              </button>
             </div>
           </div>
         </div>
